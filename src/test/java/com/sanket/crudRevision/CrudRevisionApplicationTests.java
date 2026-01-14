610fb17aa2e2294e51809571629c6369aa129b48
Sanket Gupte <sanket.gupte542@gmail.com>
2026-01-14 22:15:37 +0530

    Initial Commit

---
+++ src/test/java/com/sanket/crudRevision/CrudRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.crudRevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
